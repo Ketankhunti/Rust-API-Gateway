b67c3478b11147fed629b2621bb9442a8447af6b
ketan5050 <khuntiketan50@gmail.com>
2025-08-03 16:20:05 +0530

    cleanup

--- examples/server.rs
+++ examples/server.rs
@@ -1,9 +1,6 @@
-use std::sync::Arc;
-use api_gateway::{config::{ApiKeyStore, GatewayConfig, SecretsConfig}, utils::config_path::Cli};
-use tracing::{info, Level};
-use tokio::sync::RwLock;
+use api_gateway::{utils::config_path::Cli};
 use api_gateway::run;
-use dotenvy::dotenv;
+
 use clap::Parser;
 
 #[tokio::main]
